8df97e1e1dfeda7b6b4ca3a37724581701c4969a
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-29 23:41:10 +0530

    added variable_scope.cpp

---
+++ variable_scope.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int global_scope = 7;
+
+void print_global(){
+    cout << "Variable global_scope has value: " << global_scope << "\n";
+}
+
+int main(){
+    int global_scope = 17;
+    cout << "Variable global_scope has value: " << global_scope << "\n";
+    print_global();
+    return 0;
+}
