065fb9e177f0b1d384c0f4651f355d58b342ea93
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-29 22:44:01 +0530

    added variables_comments.cpp

---
+++ variables_comments.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+/*
+1st line
+This is a multi line comment.
+last line
+*/
+
+// Example comment 
+int main(){
+    int var1 = 7;
+    float var2 = 7.0;
+    cout << "Variables and Comments " << var1;
+    cout << var2;
+    return 0;
+}
