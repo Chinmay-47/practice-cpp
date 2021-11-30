6c27e9dea491f6661b2fddd2fdc22a433cd5a2bd
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-30 18:33:57 +0530

    added headers_operators.cpp and headers_operators.h

---
+++ src/headers_operators.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+/*
+Types of header files:
+-> System header files - Shipped with compiler
+-> User defined header files - Defined by user
+*/
+
+// Example of user defined header file.
+#include "headers_operators.h"
+
+// Check out cppreference in google to know more on system header files
+
+int main()
+{
+    // endl - End line operator
+    cout << "End line Operator in C++" << endl;
+    cout << endl;
+
+
+    // Arithmetic operators
+    int a = 7, b = 3;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+    cout << "a++ = " << a++ << endl; // Increments after printing
+    cout << "a-- = " << a-- << endl; // Decrements after printing
+    cout << "++a = " << ++a << endl; // Increments before printing
+    cout << "--a = " << --a << endl; // Decrements before printing
+    cout << endl;
+
+
+    // Comparison operators
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "a == b = " << (a == b) << endl;
+    cout << "a != b = " << (a != b) << endl;
+    cout << "a > b = " << (a > b) << endl;
+    cout << "a < b = " << (a < b) << endl;
+    cout << "a >= b = " << (a == b) << endl;
+    cout << "a <= b = " << (a == b) << endl;
+    cout << endl;
+
+    // Logical Operators
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "((a == b) && (a > b)) = " << ((a == b) && (a > b)) << endl; // Logical AND
+    cout << "((a > b) && (b < a)) = " << ((a > b) && (b < a)) << endl;
+    cout << "((a == b) || (b < a)) = " << ((a == b) || (b < a)) << endl; // Logical OR
+    cout << "(!(a == b)) = " << (!(a == b)) << endl; // Logical Not
+    cout << endl;
+    
+    return 0;
+}
