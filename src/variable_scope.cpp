68914bd43322434419b7b6019de0e17b9bdafc3c
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-30 23:53:48 +0530

    minor

--- src/variable_scope.cpp
+++ src/variable_scope.cpp
@@ -9,6 +9,8 @@ void print_global(){
 
 int main(){
     int global_scope = 17;
+
+    cout << "Variable global_scope has value: " << :: global_scope << "\n";
     cout << "Variable global_scope has value: " << global_scope << "\n";
     print_global();
     return 0;
