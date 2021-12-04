54f3fda89338d702b301095943a0e4cbd99d829f
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-12-04 23:58:42 +0530

    updated pointers.cpp

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -7,9 +7,18 @@ int main(){
     int a = 3;
     int* b = &a;
 
-    cout << a << endl;
-    cout << b << endl;
-    cout << *b << endl;
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+    cout << "Address of a: " << &a << endl;
+    cout << "The value at address b: " << *b << endl;
+
+    int** c = &b;
+    // Pointer c contains address to pointer b
+
+    cout << "Value of c: " << c << endl;
+    cout << "Address of b: " << &b << endl;
+    cout << "The value at address c: " << *c << endl;
+    cout << "The value at address b from c: " << **c << endl;
 
     return 0;
 }
