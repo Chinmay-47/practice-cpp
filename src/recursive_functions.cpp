12d3ea6be947cb703c114fcbd0d9b355a7c9b16c
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-12-20 23:51:05 +0530

    updated recursive_functions.cpp

--- src/recursive_functions.cpp
+++ src/recursive_functions.cpp
@@ -2,17 +2,31 @@
 
 using namespace std;
 
-int nth_fibbonaci_num(int num){
+int nth_fibonacci_num(int num){
     if ((num == 0) || (num == 1)){
         return num;
     }
-    return nth_fibbonaci_num(num - 1) + nth_fibbonaci_num(num - 2);
+    return nth_fibonacci_num(num - 1) + nth_fibonacci_num(num - 2);
+}
+
+int factorial(int num){
+    if ((num == 0) || (num == 1)){
+        return 1;
+    }
+    return num * factorial(num - 1);
 }
 
 int main(){
 
+    cout << "Fibonacci Sequence:" << endl;
     for(int i = 0; i < 20; i++){
-        cout << nth_fibbonaci_num(i) << endl;
-    }    
+        cout << nth_fibonacci_num(i) << endl;
+    }
+
+    cout << "Factorials:" << endl;
+    for(int i = 0; i < 10; i++){
+        cout << factorial(i) << endl;
+    }
+
     return 0;
 }
