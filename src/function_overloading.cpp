b0af761cf854df08ab2a13cb289808820a9efbba
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-12-21 01:32:24 +0530

    updated function_overloading.cpp

--- src/function_overloading.cpp
+++ src/function_overloading.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+int add(int a, int b){
+    return a + b;
+}
+
+int add(int a, int b, int c){
+    return a + b + c;
+}
+
+
 int main(){
 
-    
+    cout << "Sum1: " << add(7, 8) << endl;
+    cout << "Sum2: " << add(7, 8, 5) << endl;
+
     return 0;
 }
