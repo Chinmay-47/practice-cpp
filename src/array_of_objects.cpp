3e0b937057ea675d9da4aad05c16703f821d30f4
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-01-01 22:30:07 +0530

    updated array_of_objects.cpp

---
+++ src/array_of_objects.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Car
+{
+    string car_brand;
+    int car_price;
+
+public:
+    void add_car_details(void)
+    {
+        cout << "Enter car brand: " << endl;
+        cin >> car_brand;
+        cout << "Enter car price: " << endl;
+        cin >> car_price;
+    }
+
+    void show_car_details(void)
+    {
+        cout << "Car of brand " << car_brand << " costs " << car_price << endl;
+    }
+};
+
+int main()
+{
+
+    Car cars[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        cars[i].add_car_details();
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        cars[i].show_car_details();
+    }
+
+    return 0;
+}
